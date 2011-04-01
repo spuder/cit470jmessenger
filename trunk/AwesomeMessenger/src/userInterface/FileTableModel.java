161260df17829f14217596361aaa06cc6a678e21
boisebartian@gmail.com <boisebartian@gmail.com@78d72696-2cf0-c8c7-4c1b-f67787842f89>
2011-04-01 17:44:08 +0000

    initial commit

---
+++ trunk/AwesomeMessenger/src/userInterface/FileTableModel.java
@@ -0,0 +1,74 @@
+package userInterface;
+
+
+import java.util.Vector;
+
+
+import javax.swing.table.AbstractTableModel;
+
+
+public class FileTableModel extends AbstractTableModel {
+
+
+private Vector<String> columnNames;
+
+private Vector<String[]> files;
+
+public FileTableModel(Vector files, Vector columnNames){
+
+this.columnNames = columnNames;
+
+this.files = files;
+
+}
+
+public String getColumnName(int col){
+
+return columnNames.get(col);
+
+}
+
+public boolean isCellEditable(int row, int col){ 
+
+return false; 
+
+}
+
+ 
+
+public void setValueAt(String value, int row, int col) {
+
+(files.get(row))[col] = value;
+
+fireTableCellUpdated(row, col);
+
+}
+
+@Override
+
+public int getColumnCount() {
+
+return columnNames.size();
+
+}
+
+
+@Override
+
+public int getRowCount() {
+
+return files.size();
+
+}
+
+
+@Override
+
+public Object getValueAt(int vectorIndex, int arrayIndex) {
+
+return (files.get(vectorIndex))[arrayIndex];
+
+}
+
+
+}
