75fdf1ada6a218a90e29eaa7d4726cffbd319862
skyler.onken@gmail.com <skyler.onken@gmail.com@78d72696-2cf0-c8c7-4c1b-f67787842f89>
2011-04-02 17:10:41 +0000

    Server listener added and commited

---
+++ trunk/JMessengerServer/src/beans/CommunicationBean.java
@@ -0,0 +1,25 @@
+package beans;
+
+import java.io.Serializable;
+import java.util.HashMap;
+
+public class CommunicationBean implements Serializable {
+	private String command;
+	private HashMap parameters;
+	
+	public void setCommand(String command) {
+		this.command = command;
+	}
+
+	public String getCommand() {
+		return command;
+	}
+
+	public void setParameters(HashMap parameters) {
+		this.parameters = parameters;
+	}
+
+	public HashMap getParameters() {
+		return parameters;
+	}
+}
