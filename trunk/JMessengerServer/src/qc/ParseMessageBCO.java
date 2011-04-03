cf2d52074c369c3a75ca289bc895b2409d458f2a
skyler.onken@gmail.com <skyler.onken@gmail.com@78d72696-2cf0-c8c7-4c1b-f67787842f89>
2011-04-03 23:40:11 +0000

    messaging functionality

---
+++ trunk/JMessengerServer/src/qc/ParseMessageBCO.java
@@ -0,0 +1,46 @@
+package qc;
+
+import java.io.ObjectOutputStream;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.quickconnect.ControlObject;
+
+import ui.MainFrame;
+
+import com.mysql.jdbc.Connection;
+
+import beans.MessageBean;
+import beans.SessionBean;
+
+public class ParseMessageBCO implements ControlObject {
+
+	@Override
+	public Object handleIt(ArrayList<Object> arg0) {
+
+		HashMap params = (HashMap) arg0.get(1);
+		MessageBean msg = (MessageBean) params.get("message");
+		
+		Connection con = (Connection) MainFrame.mainFrame.getController().getConnectionPool().getConnection();
+		java.sql.PreparedStatement select = null;
+		ResultSet results = null;
+		
+		try {
+			select = con.prepareStatement("INSERT Message (UserID,SessionID,MessageSendTime,Message) "
+					+ "SELECT UserID, ?, ?, ? FROM User WHERE UserName = ?");
+			select.setString(1, msg.getSessionid());
+			select.setTimestamp(2, msg.getTimestamp());
+			select.setString(3, msg.getMessage());
+			select.setString(4, msg.getSender());
+			select.execute();
+		} 
+		catch (SQLException e1) {
+			e1.printStackTrace();
+		}
+		
+		return msg;
+	}
+
+}
