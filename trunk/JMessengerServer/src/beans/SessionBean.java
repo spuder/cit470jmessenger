431b9cdd1f34d38f0342b07714ea912182b4cfe7
skyler.onken@gmail.com <skyler.onken@gmail.com@78d72696-2cf0-c8c7-4c1b-f67787842f89>
2011-04-03 17:44:17 +0000

    updated sessionbean

--- trunk/JMessengerServer/src/beans/SessionBean.java
+++ trunk/JMessengerServer/src/beans/SessionBean.java
@@ -2,21 +2,24 @@ package beans;
 
 import java.io.Serializable;
 import java.sql.Timestamp;
+import java.util.ArrayList;
 
 public class SessionBean implements Serializable {
 
-	private static final long serialVersionUID = 973620567644323290L;
-
+	private static final long serialVersionUID = 2108122228047431995L;
+	
 	static int session_number = 0;
 	
 	String sessionId;
 	String sessionName;
 	Timestamp creationTime;
+	ArrayList sessionUsers;
 	
 	public SessionBean(){
 		creationTime = new Timestamp(System.currentTimeMillis());
 		this.sessionId = "" + SessionBean.session_number++ + "-" + creationTime.toString();
 		this.sessionName = "Session " + this.sessionId;
+		this.sessionUsers = new ArrayList();
 	}
 	
 	public SessionBean(String name){
@@ -38,6 +41,14 @@ public class SessionBean implements Serializable {
 
 	public void setSessionName(String sessionName) {
 		this.sessionName = sessionName;
+	}
+
+	public ArrayList getSessionUsers() {
+		return sessionUsers;
+	}
+
+	public void setSessionUsers(ArrayList sessionUsers) {
+		this.sessionUsers = sessionUsers;
 	}
 	
 }
