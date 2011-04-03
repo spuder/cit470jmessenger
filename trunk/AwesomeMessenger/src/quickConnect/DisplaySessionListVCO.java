d86248d9b937d2d197e2d709a7965b4f0ef86d5e
skyler.onken@gmail.com <skyler.onken@gmail.com@78d72696-2cf0-c8c7-4c1b-f67787842f89>
2011-04-03 20:14:41 +0000

    Added functionality to join session. including the displaying of all active sessions

---
+++ trunk/AwesomeMessenger/src/quickConnect/DisplaySessionListVCO.java
@@ -0,0 +1,82 @@
+package quickConnect;
+
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Vector;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.WindowConstants;
+
+import org.quickconnect.ControlObject;
+import org.quickconnect.QuickConnect;
+
+import userInterface.StandardTableModel;
+
+public class DisplaySessionListVCO implements ControlObject {
+
+	JDialog dialog;
+	JTable table;
+	Vector list;
+	
+	@Override
+	public Object handleIt(ArrayList<Object> arg0) {
+
+		dialog = new JDialog();
+		HashMap params = (HashMap) arg0.get(0);
+		list = (Vector) params.get("list");
+		Vector columns = new Vector();
+		columns.add("Session");
+		columns.add("ID");
+		
+		StandardTableModel tableModel = new StandardTableModel(list, columns);
+		table = new JTable(tableModel);
+		JScrollPane scroll = new JScrollPane(table);
+		scroll.setPreferredSize(new Dimension(300,200));
+		
+		JButton button = new JButton("Join");
+		JButton cancel = new JButton("Cancel");
+		
+		button.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				ArrayList params = new ArrayList();
+				String[] row = (String[]) list.get(table.getSelectedRow());
+				params.add((String)row[1]);
+				QuickConnect.handleRequest("joinSession", params);
+				dialog.setVisible(false);
+			}
+		});
+		
+		cancel.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				dialog.setVisible(false);
+			}	
+		});
+		
+		JPanel panel = new JPanel();
+		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+		panel.add(scroll);
+		panel.add(button);
+		panel.add(cancel);
+		
+		dialog.setTitle("Select Chat To Join:");
+		dialog.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+		
+		dialog.getContentPane().add(panel);
+		
+		dialog.pack();
+		dialog.setVisible(true);
+		
+		return null;
+	}
+
+}
---
+++ trunk/AwesomeMessenger/src/userInterface/StandardTableModel.java
@@ -0,0 +1,54 @@
+package userInterface;
+
+import java.util.Vector;
+
+import javax.swing.table.AbstractTableModel;
+
+public class StandardTableModel extends AbstractTableModel {
+
+
+	private Vector<String> columnNames;
+	private Vector<String[]> items;
+
+	public StandardTableModel(Vector<String[]> items, Vector<String> columnNames){
+		this.columnNames = columnNames;
+		this.items = items;
+	}
+
+	@Override
+	public String getColumnName(int col){
+		return columnNames.get(col);
+	}
+
+	@Override
+	public boolean isCellEditable(int row, int col){ 
+		return false; 
+	}
+
+	public void setValueAt(String value, int row, int col) {
+		(items.get(row))[col] = value;
+		fireTableCellUpdated(row, col);
+	}
+
+	@Override
+	public int getColumnCount() { return columnNames.size(); }
+
+	@Override
+	public int getRowCount() { return items.size(); }
+
+	@Override
+	public Object getValueAt(int vectorIndex, int arrayIndex) {
+		return (items.get(vectorIndex))[arrayIndex];
+	}
+
+	public Vector<String[]> getItems() {
+		return items;
+	}
+
+	public void setItems(Vector<String[]> items) {
+		this.items = items;
+		this.fireTableDataChanged();
+	}
+	
+
+}
